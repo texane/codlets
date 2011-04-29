d7e06d0f690e5f87e60a7c9c511622a7371381ae
texane <texane@dell.fbx.proxad.net>
2011-04-29 18:35:40 -0500

    [add] graphics/stl stl header file

---
+++ graphics/stl/src/stl.h
@@ -0,0 +1,30 @@
+#ifndef STL_H_INCLUDED
+# define STL_H_INCLUDED
+
+
+typedef double double_type;
+
+/* stl facet container */
+
+typedef struct stl_list_elem
+{
+  struct stl_list_elem* next;
+  double_type normal[3];
+  double_type vertices[9]; /* ordered x0, y0 ... */
+} stl_list_elem_t;
+
+
+typedef struct stl_list
+{
+  unsigned int count;
+  stl_list_elem_t* head;
+  stl_list_elem_t* tail;
+} stl_list_t;
+
+
+int stl_read_file(const char*, stl_list_t*);
+void stl_list_to_soa(stl_list_t*, double_type*);
+void stl_free_list(stl_list_t*);
+
+
+#endif /* ! STL_H_INCLUDED */
