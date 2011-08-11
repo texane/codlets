dea1aacbe696225167f71d0507b9b9df247fc198
Fabien Le Mentec <texane@gmail.com>
2011-08-11 16:07:02 -0500

    [update] least square error, higher degrees

--- maths/lse/main.m
+++ maths/lse/main.m
@@ -1,17 +1,32 @@
 # least square error method
 # http://ceee.rice.edu/Books/LA/leastsq/index.html
 
-data = dlmread("dat.csv", " ");
+# data = dlmread("dat.csv", " ");
+data = dlmread("parabola.csv", " ");
+
 x = data(:,1);
-X = [ones(size(data)(1), 1)'; x' ]';
+X = [ones(size(data)(1), 1)'];
+
+degree = 1;
+for d = 1:degree
+  X = [ X; (x.^d)'];
+end
+X = X';
+
+
 Y = data(:,2);
 
 # solve the normal eq: X' * X * c = X' * y
 c = (X' * X) \ (X' * Y);
 
 # print the resulting linear equation
-printf("x * %f + %f", c(2), c(1));
+# printf("x^2 * %f x * %f + %f", c(3), c(2), c(1));
 
 # plot results
-plot(x, Y, '.', x * c(2) + c(1));
+y = c(1);
+for d = 1:degree
+  y = y + x.^d * c(d + 1);
+end
+
+plot(x, Y, '.', y);
 waitforbuttonpress;
