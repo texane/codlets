efa74cd5ec5ab86371806ae6ae3751c39b0a6c71
Fabien Le Mentec <texane@gmail.com>
2011-08-11 15:09:15 -0500

    [update] least square error

--- maths/lse/main.m
+++ maths/lse/main.m
@@ -2,16 +2,16 @@
 # http://ceee.rice.edu/Books/LA/leastsq/index.html
 
 data = dlmread("dat.csv", " ");
-X = [ones(size(data)(1), 1)'; data(:,1)' ]';
+x = data(:,1);
+X = [ones(size(data)(1), 1)'; x' ]';
 Y = data(:,2);
 
-# solve X' * X * c = X' * y
+# solve the normal eq: X' * X * c = X' * y
 c = (X' * X) \ (X' * Y);
 
 # print the resulting linear equation
 printf("x * %f + %f", c(2), c(1));
 
 # plot results
-x = data(:,1);
-plot(x, data(:,2), x * c(2) + c(1));
+plot(x, Y, '.', x * c(2) + c(1));
 waitforbuttonpress;
